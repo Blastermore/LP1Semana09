6fcba35d2b913f4718a4baca109e9d61b819f8fc
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-10 15:20:44 +0100

    Add initial reading structure

---
+++ CharSets/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CharSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string s;
+            using StreamReader sr = new StreamReader("CharSets/"+args[i]);
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
