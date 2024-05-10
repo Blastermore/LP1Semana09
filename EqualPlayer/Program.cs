13bf0af171221f261299f5f112d797176a2bd764
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-10 14:53:09 +0100

    Add variables ana1, paulo, ana2 and add each one to setOfPlayers; Add test print

--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EqualPlayer
 {
@@ -6,7 +7,20 @@ namespace EqualPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player ana1 = new Player(PlayerClass.Tank, "Ana" );
+            Player paulo = new Player(PlayerClass.Slayer, "Paulo" );
+            Player ana2 = new Player(PlayerClass.Tank, "Ana" );
+
+
+            HashSet<Player> setOfPlayers = new HashSet<Player>();
+            setOfPlayers.Add(ana1);
+            setOfPlayers.Add(paulo);
+            setOfPlayers.Add(ana2);
+
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
         }
     }
 }
