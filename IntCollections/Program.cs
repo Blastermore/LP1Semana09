716ac0d3bc8d0b78e370401099efccb66dc43f39
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-10 13:27:41 +0100

    Initialize List, Stack, Queue and HashSet collections

---
+++ IntCollections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(){1, 10, -30, 10, -5}
+            Stack<int> stack = new Stack<int>(){1, 10, -30, 10, -5}
+            Queue<int> queue = new Queue<int><int>(){1, 10, -30, 10, -5}
+            HashSet<int> hashSet = new HashSet<int>(){1, 10, -30, 10, -5}
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
