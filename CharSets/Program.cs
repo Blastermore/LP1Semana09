0f63b763d16a9f452cc4344722c914e10640a196
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-10 15:28:34 +0100

    Complete Reader

--- CharSets/Program.cs
+++ CharSets/Program.cs
@@ -9,12 +9,20 @@ namespace CharSets
     {
         static void Main(string[] args)
         {
-            int i = 0;
             string s;
-            using StreamReader sr = new StreamReader("CharSets/"+args[i]);
-            while ((s = sr.ReadLine()) != null)
+            for(int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine(s);
+                string path = Path.Combine("CharSets",args[i]);
+                using StreamReader sr = new StreamReader(path);
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (s.Length >1)
+                    {
+                        Console.WriteLine("Line Has more than 1 character");
+                        return;
+                    }
+                    Console.WriteLine(s);
+                }
             }
         }
     }
