1ab56f0530087ce9a9d545a0392b5b48313d1c88
Afonso Cunha <afonsocunha.dev@gmail.com>
2024-05-10 14:57:33 +0100

    Inicialize queue variable and console input question print

---
+++ FilePower1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Yipee";
+            Queue<string> queue = new Queue<string>();
+            while (s != "")
+            {
+                Console.Write("Next input\n>")
+
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
